5364e095a497b46b104d1059ca9000b9604d43d6
Rafael <rafael.uribe.2080@gmail.com>
2021-12-30 18:59:23 -0500

    Save & Delete Images

--- src/controllers/ImageController.ts
+++ src/controllers/ImageController.ts
@@ -2,20 +2,19 @@ import { firebaseStorage } from '../firebase';
 import {
   getDownloadURL,
   ref,
-  uploadBytes,
   deleteObject,
+  uploadString,
 } from 'firebase/storage';
 import { Request, Response } from 'express';
 export default class ImageController {
   public static async save(req: Request, res: Response) {
     try {
-      const { name, data } = req.body;
-      const newFile = new File(data, name);
-      const path = Date.now() + newFile.name;
+      const { name, data: base64 } = req.body;
+      const path = Date.now() + name;
       const reference = ref(firebaseStorage, path);
-      await uploadBytes(reference, newFile);
-      const url = getDownloadURL(reference);
-      res.send({ url });
+      await uploadString(reference, base64, 'data_url');
+      const data = await getDownloadURL(reference);
+      res.send({ data, message: '' });
     } catch (e: any) {
       res.send({ message: e.message });
     }
@@ -26,7 +25,7 @@ export default class ImageController {
       const path = url.substr(url.lastIndexOf('/') + 1);
       const reference = ref(firebaseStorage, path);
       await deleteObject(reference);
-      res.send({ message: 'The image was deleted' });
+      res.send({ data: '', message: 'The image was deleted' });
     } catch (e: any) {
       res.send({ message: e.message });
     }
